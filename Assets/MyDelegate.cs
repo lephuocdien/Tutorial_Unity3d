4eb2a4cd5214083addd33596a47a9e647fb1a60f
lephuocdien <ledientesttb@gmail.com>
2020-03-27 13:27:44 +0700

    +demo

---
+++ Assets/MyDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDelegate : MonoBehaviour
+{
+    delegate void MyDelegateok();
+    MyDelegateok myDelegate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDelegate += powerUp;
+      
+
+        myDelegate += turnRedcolor;
+        myDelegate();
+        
+    }
+    public void powerUp()
+    {
+        Debug.Log("Rising power up :");
+    }
+    public void turnRedcolor()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
